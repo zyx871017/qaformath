4eb9d84c06284afa58fce3e0a45382d098c5ab1e
zhaoyuxiang <112408584@qq.com>
2017-08-30 12:02:23 +0800

    样式改动，调整了添加了地址管理页

--- pages/common/config/common.js
+++ pages/common/config/common.js
@@ -1,3 +1,36 @@
+function getToken() {
+  const app = getApp();
+  const promises = [];
+  const getUserInfo = new Promise(function(resolve, reject) {
+    wx.getUserInfo({
+      success:function(res) {
+        resolve(res);
+      },
+      fail: function(res) {
+        reject(res);
+      }
+    });
+  });
+  promises.push(getUserInfo);
+  const checkSession = new Promise(function(resolve, reject) {
+    wx.checkSession({
+      success: function(res) {
+        resolve(res);
+      },
+      fail: function(res) {
+        reject(res);
+      }
+    });
+  });
+  promises.push(checkSession);
+  Promise.all(promises).then(function(res){
+    console.log(res);
+  }).catch(function(res) {
+    console.log(res);
+  })
+}
+
+
 module.exports.common = {
   apiPrefix: 'https://www.qaformath.com/zbuniserver-api',
   appSecret: '4f13ffae3aecc109e75c94d34af71ea9'
